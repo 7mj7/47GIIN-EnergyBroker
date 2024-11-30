64baea76757fab5a3a4367d3612e240c860b7c5e
Manuel Andreu <Manuel.andreu@gmail.com>
2024-11-30 18:01:21 +0100

    Añado las validaciones

--- app/Filament/Admin/Pages/Parametros.php
+++ app/Filament/Admin/Pages/Parametros.php
@@ -24,15 +24,13 @@ class Parametros extends Page
 
     // Definir una propiedad para almacenar los datos del formulario
     public ?array $parametrosFiscales = [];
-    //public ?array $parametrosCorreo = [];
+    public ?array $parametrosContacto = [];
 
     public function mount()
     {
         // Cargar configuraciones y organizarlas en arrays
         $this->parametrosFiscales = Parametro::getByGrupo('fiscal');
-
-        //$this->parametrosCorreo = Parametro::getByGrupo('correo');
-
+        $this->parametrosContacto = Parametro::getByGrupo('contacto');
     }
 
     protected function getFormSchema(): array
@@ -68,22 +66,29 @@ class Parametros extends Page
                         ->statePath('parametrosFiscales.provincia'),
                 ])->columns(3),
 
-            /*
-            Section::make('Configuraciones de Correo')
+
+            Section::make('Datos de Contacto')
                 ->schema([
-                    TextInput::make('parametrosCorreo.correo_envio')
-                        ->label('Correo Electrónico de Envío')
-                        ->statePath('parametrosCorreo.correo_envio'),
+                    TextInput::make('parametrosContacto.telefono')
+                        ->label('Teléfono')
+                        ->tel()
+                        ->regex('/^(?:\+34|0034)?[6-9][0-9]{8}$/')
+                        ->validationMessages([
+                            'regex' => 'Debe ser un número español válido (opcional +34 o 0034, seguido de 9 dígitos)',
+                        ])
+                        ->helperText('Ejemplos: 666777888, +34666777888, 0034666777888')
+                        ->autocomplete(false)
+                        ->statePath('parametrosContacto.telefono'),
+                    TextInput::make('parametrosContacto.email')
+                        ->label('Email')
+                        ->email()
+                        ->autocomplete(false)
+                        ->statePath('parametrosContacto.email'),
+                ])->columns(2),
 
-                    TextInput::make('parametrosCorreo.smtp_servidor')
-                        ->label('Servidor SMTP')
-                        ->statePath('parametrosCorreo.smtp_servidor'),
 
-                    TextInput::make('parametrosCorreo.smtp_puerto')
-                        ->label('Puerto SMTP')
-                        ->statePath('parametrosCorreo.smtp_puerto'),
-                ]),
-            */
+
+
 
             \Filament\Forms\Components\Actions::make([
                 \Filament\Forms\Components\Actions\Action::make('Guardar Configuraciones')
@@ -94,20 +99,75 @@ class Parametros extends Page
         ];
     }
 
+
+    public  function getRules(): array
+    {
+        return [
+            'parametrosContacto.email' => [
+                'required',
+                'regex:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/',
+                'max:255'
+            ],
+            'parametrosContacto.telefono' => [
+                'required',
+                'regex:/^(?:\+34|0034)?[6-9][0-9]{8}$/'
+            ],
+            'parametrosFiscales.nombre' => [
+                'required',
+                'string',
+                'min:3',
+                'max:255'
+            ],
+            'parametrosFiscales.nif' => [
+                'required',
+                'string',
+                'regex:/^([ABCDEFGHJKLMNPQRSUVW])(\d{7})([0-9A-J])$|^([0-9]{8})([A-Z])$/'
+            ],
+            'parametrosFiscales.codigo_postal' => [
+                'required',
+                'regex:/^[0-9]{5}$/'
+            ]
+        ];
+    }
+
+    public function getValidationMessages(): array
+    {
+        return [
+            // Parametros Fiscales            
+            'parametrosFiscales.nombre.min' => 'El nombre debe tener al menos 3 caracteres', // Mensaje personalizado para min
+            'parametrosFiscales.nif.regex' => 'El NIF/CIF debe tener un formato valido',
+            'parametrosFiscales.codigo_postal.regex' => 'El código postal debe tener 5 dígitos',
+
+            // Parametros de Contacto
+            //'parametrosContacto.email.required' => 'El email es obligatorio',
+            'parametrosContacto.email.regex' => 'El formato del email no es válido',
+            'parametrosContacto.telefono.regex' => 'El teléfono debe ser un número español válido',
+    
+        ];
+    }
+
+
+
     // Método para guardar las configuraciones
     public function saveSettings()
     {
+        $this->validate(
+            $this->getRules(),
+            $this->getValidationMessages()
+        );
+
+        // Guardar los parámetros Fiscales
         foreach ($this->parametrosFiscales as $nombre => $valor) {
             // Actualizar o crear el parámetro
             Parametro::setParametro('fiscal', $nombre, $valor);
         }
 
-        /*
-        foreach ($this->parametrosCorreo as $nombre => $valor) {
-                // Actualizar o crear el parámetro
-            Parametro::setParametro('correo', $nombre, $valor);
+        // Guardar los parámetros de Contacto
+        foreach ($this->parametrosContacto as $nombre => $valor) {
+            // Actualizar o crear el parámetro
+            Parametro::setParametro('contacto', $nombre, $valor);
         }
-        */
+
 
         \Filament\Notifications\Notification::make()
             ->title('Configuraciones actualizadas correctamente.')
