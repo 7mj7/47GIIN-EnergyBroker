3e5f59761eec05baf459dfb759a613bdf7e654ae
Manuel Andreu <Manuel.andreu@gmail.com>
2024-12-04 20:29:49 +0100

    CRUD para los estados de los contratos

---
+++ app/Filament/Admin/Resources/EstadoContratoResource/Pages/CreateEstadoContrato.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Admin\Resources\EstadoContratoResource\Pages;
+
+use App\Filament\Admin\Resources\EstadoContratoResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateEstadoContrato extends CreateRecord
+{
+    protected static string $resource = EstadoContratoResource::class;
+}
